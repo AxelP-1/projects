b37ec69a91d84e08554f1ee95255ef08a04ecde3
AxelP-1 <145084841+AxelP-1@users.noreply.github.com>
2025-10-03 19:33:39 +0200

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 col = {1: 0}
 
 def calc(x):
@@ -12,9 +15,15 @@ def calc(x):
 
 mx = -1
 b = 0
+rcrdbrk=[]
 while True:
     b += 1
     steps = calc(b)
     if steps > mx:
         mx = steps
+        rcrdbrk.append(b)
         print(f"n={b}: {steps}")
+        """
+        plt.plot(rcrdbrk)
+        plt.show()
+        """
