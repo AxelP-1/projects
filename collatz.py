5dc494212bed4bdf5bfe34bcb788cb6297d8540d
AxelP-1 <145084841+AxelP-1@users.noreply.github.com>
2025-10-03 19:25:04 +0200

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,20 @@
+col = {1: 0}
+
+def calc(x):
+    if x in col:
+        return col[x]
+    if x % 2 == 0:
+        next_x = x // 2
+    else:
+        next_x = 3 * x + 1
+    col[x] = calc(next_x) + 1
+    return col[x]
+
+mx = -1
+b = 0
+while True:
+    b += 1
+    steps = calc(b)
+    if steps > mx:
+        mx = steps
+        print(f"n={b}: {steps}")
